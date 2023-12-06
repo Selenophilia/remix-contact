eebcf3ea5a059bb3e268a6bb5bed11ddd364c95c
Selenophilia <pauloargenal@gmail.com>
2023-12-06 13:01:14 +0800

    Code cleanup

--- app/contacts/utils/get-nav-class.ts
+++ app/contacts/utils/get-nav-class.ts
@@ -1,10 +1,9 @@
 interface LinkParams {
   isActive: boolean;
-  isPending: boolean;
 }
 
 export const getNavClass = (params: LinkParams) => {
-  const { isActive, isPending } = params;
+  const { isActive } = params;
   let activeClass = "bg-blue-20 text-white w-full rounded-md  ";
   let linkClass = "pl-2 flex justify-between py-2 pr-4";
 
